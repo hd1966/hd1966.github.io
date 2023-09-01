daad1707168d20340621da2c08e098a493c31aa9
hd1966 <32216299+hd1966@users.noreply.github.com>
2023-09-01 13:26:08 +0200

    leaflet123

---
+++ leaflet/geojson.js
@@ -0,0 +1,61 @@
+var objjson = {"type":"FeatureCollection",
+"features":[
+    {"type":"Feature",
+    "properties":{
+           "title": "Stadsparken",
+           "description":"Leka i stadsparken.",
+           "logo":"stads.png",
+           "style": {
+            weight: 2,
+            color: "#999",
+            opacity: 1,
+            fillColor: "#B0DE5C",
+            fillOpacity: 0.8
+           }
+    },
+    "geometry":{"type":"Polygon",
+               "coordinates":[[[13.183937072753906,55.69882429504726],
+               [13.186254501342773,55.69819548930122],
+               [13.186168670654297,55.69930798489143],
+               [13.184709548950195,55.6995981959248],
+               [13.183937072753906,55.69882429504726]]]}},
+    {"type":"Feature",
+    "properties":{
+        "title": "Central station",
+          "description":"Ta tåget till Malmö.",
+          "logo":"cs.png",
+          "style": {
+           weight: 2,
+           color: "#999",
+           opacity: 1,
+           fillColor: "#11DE00",
+           fillOpacity: 0.9
+      }
+    },
+    "geometry":{"type":"Polygon",
+               "coordinates":[[[13.185653686523438,55.70632081501279],
+                [13.18702697753906,55.70632081501279],
+                [13.18702697753906,55.70704620837462],
+                [13.185653686523438,55.70704620837462],
+                [13.185653686523438,55.70632081501279]]]}},
+       {"type":"Feature",
+    "properties":{
+         "title": "Mårtenstorg",
+          "description":"Äta någonting gott i olika restaurant.",
+          "logo":"marten.png",
+          "style": {
+            weight: 3,
+            color: "#099",
+            opacity: 1,
+            fillColor: "#DD00DD",
+            fillOpacity: 0.6
+    }
+   },
+    "geometry":{"type":"Polygon",
+                "coordinates":[[[13.193464279174805,55.70201653740479],
+                   [13.194923400878906,55.70119431809103],
+                   [13.196210861206055,55.70182307559308],
+                   [13.195266723632812,55.70235509327093],
+                   [13.193464279174805,55.70201653740479]
+              ]]}}
+]};
